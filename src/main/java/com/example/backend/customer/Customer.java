91ae2b1dd09f15116e1593761eb84a11a7beddcd
Lucas <lucasvalenteds@gmail.com>
2020-07-26 17:37:45 -0300

    Implementing the source code
    * Domain layer (Entity)
    * Persistence layer (Repository)
    * Web layer (Controller)

---
+++ src/main/java/com/example/backend/customer/Customer.java
@@ -0,0 +1,52 @@
+package com.example.backend.customer;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotEmpty
+    @NotBlank
+    private String name;
+
+    public Customer() {
+    }
+
+    public Customer(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            '}';
+    }
+}
---
+++ src/main/java/com/example/backend/customer/CustomerController.java
@@ -0,0 +1,65 @@
+package com.example.backend.customer;
+
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final CustomerRepository repository;
+
+    @Autowired
+    public CustomerController(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody @Valid Customer customer) {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(repository.save(customer));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> findAllCustomer(Pageable pageable) {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(repository.findAll(pageable).getContent());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> findOneCustomer(@PathVariable("id") long id) {
+        return repository.findById(id)
+            .map(customer ->
+                ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(customer)
+            )
+            .orElseGet(() ->
+                ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(null)
+            );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable("id") long id) {
+        repository.deleteById(id);
+
+        return ResponseEntity
+            .status(HttpStatus.ACCEPTED)
+            .body(null);
+    }
+}
