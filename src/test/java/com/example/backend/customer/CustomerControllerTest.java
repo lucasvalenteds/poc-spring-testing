a2a72f0dd6d7acf5a3c7400b4d54e66c3078e132
Lucas <lucasvalenteds@gmail.com>
2020-07-26 18:41:34 -0300

    Adding tests to Web

---
+++ src/test/java/com/example/backend/customer/CustomerControllerTest.java
@@ -0,0 +1,128 @@
+package com.example.backend.customer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(value = CustomerController.class)
+@AutoConfigureDataJpa
+class CustomerControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @MockBean
+    private CustomerRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testCreatingValidCustomer() throws Exception {
+        Customer customerWithName = createCustomerWithName();
+        Customer customerWithId = createCustomerWithId();
+
+        Mockito.when(repository.save(customerWithName))
+            .thenReturn(customerWithId);
+
+        String responseBody = mvc.perform(post("/customers")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(customerWithName)))
+            .andExpect(status().isCreated())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+        assertEquals(mapper.readValue(responseBody, Customer.class), customerWithId);
+        Mockito.verify(repository).save(customerWithName);
+    }
+
+    @Test
+    void testFindingValidCustomerById() throws Exception {
+        Customer customerWithId = createCustomerWithId();
+
+        Mockito.when(repository.findById(customerWithId.getId()))
+            .thenReturn(Optional.of(customerWithId));
+
+        String responseBody = mvc.perform(get("/customers/" + customerWithId.getId())
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+        assertEquals(mapper.readValue(responseBody, Customer.class), customerWithId);
+        Mockito.verify(repository).findById(customerWithId.getId());
+    }
+
+    @Test
+    void testFindingAllCustomers() throws Exception {
+        Customer customerWithId = createCustomerWithId();
+        List<Customer> customers = List.of(customerWithId);
+        PageRequest pageable = PageRequest.of(0, 1);
+        PageImpl<Customer> page = new PageImpl<>(customers);
+
+        Mockito.when(repository.findAll(pageable)).thenReturn(page);
+
+        mvc.perform(
+            get("/customers")
+                .queryParam("page", "0")
+                .queryParam("size", "1")
+        )
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$").isArray());
+
+        Mockito.verify(repository).findAll(pageable);
+    }
+
+    @Test
+    void testRemovingValidCustomer() throws Exception {
+        Customer customerWithId = createCustomerWithId();
+
+        mvc.perform(delete("/customers/" + customerWithId.getId())
+            .content(mapper.writeValueAsString(customerWithId)))
+            .andExpect(status().isAccepted());
+
+        Mockito.verify(repository).deleteById(customerWithId.getId());
+    }
+
+    private Customer createCustomerWithName() {
+        Customer customer = new Customer();
+        customer.setName("John Smith");
+        return customer;
+    }
+
+    private Customer createCustomerWithId() {
+        Customer customer = createCustomerWithName();
+        customer.setId(1L);
+        return customer;
+    }
+}
